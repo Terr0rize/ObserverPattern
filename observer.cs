376ff24161e2bf062eed319b1e474726cfe083c5
kitsune <57214464+Terr0rize@users.noreply.github.com>
2020-11-10 14:52:20 +0400

    Create observer.cs

---
+++ observer.cs
@@ -0,0 +1,139 @@
+using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace RefactoringGuru.DesignPatterns.Observer.Conceptual
+{
+    public interface IObserver
+    {        
+        void Update(ISubject subject);
+    }
+
+    public interface ISubject
+    {
+        void Add(IObserver observer);
+
+        void Delete (IObserver observer);
+
+        void Message();
+    }
+
+    public class Subject : ISubject
+    {      
+        public int State { get; set; } = -0;
+      
+        private List<IObserver> gau = new List<IObserver>();
+
+        // Методы управления подпиской.
+        public void Add(IObserver observer)
+        {
+            Console.WriteLine("Вы добавили нового наблюдателя.");
+            this.gau.Add(observer);
+        }
+
+        public void Delete(IObserver observer)
+        {
+            this.gau.Remove(observer);
+            Console.WriteLine("Вы удалили наблюдателя.");
+        }
+  
+        public void Message()
+        {
+            Console.WriteLine("\nВведите сообщение для оповещения: ");
+            string kek = Console.ReadLine();
+            Console.WriteLine("\nУведомление наблюдателей о: " + kek);
+
+            foreach (var observer in gau)
+            {
+                observer.Update(this);
+            }
+        }
+         
+        public void SomeBusinessLogic()
+        {           
+            this.State = new Random().Next(0, 10);
+
+            Thread.Sleep(15);
+
+            Console.WriteLine("Приоритет показа сообщения изменился на: " + this.State);
+            this.Message();
+        }
+    }
+
+  
+    class ConcreteObserverA : IObserver
+    {
+        public void Update(ISubject subject)
+        {
+            if ((subject as Subject).State < 10)
+            {
+                Console.WriteLine("ГИБДД 1: отреагировал на сообщение.");
+            }
+        }
+    }
+
+    class ConcreteObserverB : IObserver
+    {
+        public void Update(ISubject subject)
+        {
+            if ((subject as Subject).State < 10)
+            {
+                Console.WriteLine("ГИБДД 2: отреагировал на сообщение.");
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {         
+            var subject = new Subject();
+            var observerA = new ConcreteObserverA();         
+            var observerB = new ConcreteObserverB();            
+
+            do
+            {
+                Console.WriteLine("[1]. Добавить наблюдателя.\n[2]. Удалить наблюдателя.\n[3]. Отправить сообщение.\n[4]. Выход.");
+                int i = int.Parse(Console.ReadLine());
+                switch (i)
+                {
+                    case 1:
+                        Console.WriteLine("[1]. Добавить нового ГАИШНИКА в ГИБДД под номером 1.\n[2]. Добавить нового ГАИШНИКА в ГИБДД под номером 2.");
+                        int key = Convert.ToInt32(Console.ReadLine());
+                        if(key == 1)
+                        {
+                            subject.Add(observerA);
+                        }
+                        if(key ==2)
+                        {
+                            subject.Add(observerB);
+                        }
+                        break;
+                    case 2:
+                        Console.WriteLine("[1]. Отсоединить ГАИШНИКА в ГИБДД под номером 1.\n[2]. Отсоединить ГАИШНИКА в ГИБДД под номером 2.");
+                        int keyy = Convert.ToInt32(Console.ReadLine());
+                        if (keyy == 1)
+                        {
+                            subject.Delete(observerA);
+                        }
+                        if (keyy == 2)
+                        {
+                            subject.Delete(observerB);
+                        }
+                        break;
+                    case 3:
+                        subject.SomeBusinessLogic();
+                        break;                 
+                    case 4:
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("Ошибка.");
+                        break;
+                }
+            } while (true);
+        }
+
+    
+    }
+}
